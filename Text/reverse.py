58a21dc79ad77a091538218244b99ed41a7c6280
ff-sherpa <tterebus@gmail.com>
2014-05-01 13:56:14 -0400

    Update reverse.py
    first edit of code library in learning python.  
    
    did the reversing of the string my own way.

--- Text/reverse.py
+++ Text/reverse.py
@@ -5,7 +5,10 @@ will reverse it and print it out.
 """
 
 string = raw_input("Whatchu wanna say to me? ")
-copy = [c for c in string]
-for i in range(len(copy) / 2):
-    copy[i], copy[len(copy) - i - 1] = copy[len(copy) - i - 1], copy[i]
-print "You say %s, I say %s" % (string, ''.join(copy))
+
+# print input
+print(string)
+
+# reverse string
+# [begin:end:step] => leaving begin and end blank and -1 step reverses string
+print(string[::-1])
